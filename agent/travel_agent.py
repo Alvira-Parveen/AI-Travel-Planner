f076b77c1cc6775ddef50259d6bf4c22e83b1530
Alvira Parveen <2024270815.alvira@ug.sharda.ac.in>
2026-05-30 04:52:44 +0530

    Update travel_agent.py

--- agent/travel_agent.py
+++ agent/travel_agent.py
@@ -8,9 +8,15 @@ from tools.food_tool import get_food_recommendations
 
 
 # ---------------- GROQ CLIENT ---------------- #
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
 
 client = Groq(
-    api_key="YOUR API KEY"
+    api_key=os.getenv("GROQ_API_KEY")
+)
 )
 
 
